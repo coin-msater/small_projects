c159022a276cd6d7cec94a40a376883457b91836
Wang <d3cayyde@gmail.com>
2025-08-13 23:16:03 +0800

    blkjk3

--- blackjack/game.py
+++ blackjack/game.py
@@ -1,23 +1,22 @@
 import random
+from poker_cards import CARD_DECK, CARD_VALUES
 
 class Shoe:
     def __init__(self, n_decks):
-        self.n_decks = n_decks
-        self.deck = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'] * n_decks * 4
-    
-    def draw(self):
-        card = random.choice(self.deck)
-        self.deck.remove(card)
-        return card
+        self._n_decks = n_decks
+        self.deck = CARD_DECK * n_decks
+
+    def rand_card(self):
+        random.shuffle(self.deck)
+        return self.deck.pop()
 
     def reset(self):
-        self.deck = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'] * n_decks * 4
+        self.deck = CARD_DECK * self.n_decks
 
-class Hand:
+class Cardhandler:
     def __init__(self):
         self.cards_in_hand = []
-        self.is_split = False
-        self.card_values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': 11}
+        self._n_hands = 0
 
     def total_value(self):
         total = 0
@@ -32,14 +31,11 @@ class Hand:
 
         return total
 
-    def add_card(self, new_card):
-        self.cards_in_hand.append(new_card)
-
     def is_blackjack(self):
-        return self.total_value == 21
-
-    def is_potential_blackjack(self):
-        return len(self.cards_in_hand) == 2 and ('A' in self.cards_in_hand or self.total_value == 10)
+        return self.total_value == 21 and len(self.cards_in_hand) == 2
+    
+    def is_bust(self):
+        return self.total_value() > 21
     
     def is_soft(self):
         if 'A' not in self.cards_in_hand:
@@ -52,14 +48,159 @@ class Hand:
             return True
         
         return False
-             
-    def is_bust(self):
-        return self.total_value() > 21
+
+    def show_hands(self):
+        return self.cards_in_hand
+    
+    def num_hands(self):
+        return self.n_hands
+
+    # Actions
+
+    def hit(self, new_card, hand_index):
+        self.cards_in_hand[hand_index].append(new_card)
+
+    def stand(self):
+        return -1
+    
+    def reset(self):
+        self.cards_in_hand.clear()
+        self._n_hands = 0
+
+class Dealer(Cardhandler):
+    def __init__(self):
+        super().__init__()
+        self._is_sufficient = False
+
+    def up_card(self):
+        if len(self.cards_in_hand) > 0:
+            return self.cards_in_hand[0]
+        
+    def hit_threshold(self):
+        total_without_aces = sum(self.card_values[card] for card in self.cards_in_hand if card != 'A')
+        num_aces = self.cards_in_hand.count('A')
+
+        # Check if we can count one Ace as 11 without busting
+        if num_aces > 0 and total_without_aces + 11 + (num_aces - 1) <= 21:
+            # Soft hand total
+            soft_total = total_without_aces + 11 + (num_aces - 1)
+            return soft_total >= 17  # True = stop hitting
+        else:
+            # Hard hand total
+            hard_total = total_without_aces + num_aces
+            return hard_total >= 17  # True = stop hitting
+    
+class Player(Cardhandler):
+    def __init__(self):
+        super().__init__()
+        self._is_doubled = False
+        self.bank = 0
+    
+    def is_doubled(self):
+        return self._is_doubled
+    
+    def can_split(self, hand):
+        if len(self.cards_in_hand[hand]) != 2:
+            return False
+        card1 = self.card_values[self.cards_in_hand[0]]
+        card2 = self.card_values[self.cards_in_hand[1]]
+        return card1 == card2
+    
+    def split(self, hand):
+        if self.can_split(hand):
+            new_hand = [self.cards_in_hand[hand].pop()]
+            self._n_hands += 1
+            return new_hand
+        return None
+
+    def is_doubled(self):
+        return self._is_doubled
+    
+    def double_down():
+        self._is_doubled_ = True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
     def can_split(self):
-        pass
+        if len(self.cards_in_hand) != 2:
+            return False
+        card1 = self.card_values[self.cards_in_hand[0]]
+        card2 = self.card_values[self.cards_in_hand[1]]
+        return card1 == card2
+
+    # Actions
+
+    def add_card(self, new_card):
+        self.cards_in_hand.append(new_card)
+
     def clear(self):
         self.cards_in_hand.clear()
 
     def split(self):
-        pass
+        if self.can_split():
+            new_hand = Hand()
+            new_hand.add_card(self.cards_in_hand.pop())
+            new_hand.is_split = True
+            self.is_split = True
+            return new_hand
+        return None
+        
+class Bet:
+    def __init__(self, amount):
+        self.amount = amount
+        self.insurance = 0
+        self._is_doubled = False
+        
+    # States
+
+    def is_doubled(self):
+        return self._is_doubled
+
+    def is_surrendered(self):
+        return self._is_surrendered
+
+    # Actions
+
+    def double_down(self):
+        if not self.is_doubled:
+            self.is_doubled = True
+            self.amount *= 2
+
+    def surrender(self):
+        if not self._is_doubled and not self._is_surrendered:
+            self.amount /= 2
+            self._is_surrendered = True
+
+    def add_insurance(self, insurance_amount):
+        if insurance_amount <= self.amount / 2:
+            self.insurance = insurance_amount
+            return True
+        return False
+
+class Player:
+    def __init__(self, bankroll):
+        self.hands = []
+        self.bets = []
+        self.bankroll = bankroll
+    
+    def placebet(self):
+
+        - buy_insurance()
+        - new_card()
+        - decide_action
+        - settle (money)
+        - split
+    
